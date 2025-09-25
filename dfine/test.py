e53c73186090495597d530844ebf3de2d620aa8d
Milad Heidari <milad.h9999@gmail.com>
2025-09-25 11:42:51 -0700

    Add: prediction testing code

--- dfine/test.py
+++ dfine/test.py
@@ -1,5 +1,6 @@
 import torch
 import wandb
+import einops
 import numpy as np
 import gymnasium as gym
 from argparse import Namespace
@@ -9,6 +10,7 @@ from .models import (
     Encoder,
     Dynamics,
     CostModel,
+    Decoder,
 )
 
 
@@ -51,3 +53,52 @@ def test(
             rewards.append(total_reward)
 
     wandb.log({"rewards": wandb.Histogram(rewards)})
+
+
+def test_prediction(
+    args: Namespace,
+    encoder: Encoder,
+    decoder: Decoder,
+    dynamics_model: Dynamics,
+    y: torch.Tensor,
+    u: torch.Tensor,
+):
+    with torch.no_grad():
+        
+        F = y.shape[0]
+        B = y.shape[1]
+        T = u.shape[0]
+
+        assert T >= F, "the input sequence (u) must be at least the same length as the observation sequence (y)" 
+
+        a = encoder(einops.rearrange(y, "l b y -> (l b) y"))
+        a = einops.rearrange(a, "(l b) a -> l b a", b=B)
+
+        # initial belief over x0: N(0, I)
+        mean = torch.zeros((B, args.x_dim), device=y.device)
+        cov = torch.eye(args.x_dim, args.x_dim, device=y.device).repeat([B, 1, 1])
+
+        for t in range(1, F):
+            mean, cov = dynamics_model.dynamics_update(
+                mean=mean,
+                cov=cov,
+                u=u[t-1],
+            )
+            mean, cov = dynamics_model.measurement_update(
+                mean=mean,
+                cov=cov,
+                a=a[t],
+            )
+
+        pred_y = torch.zeros((T+1-F, B, y.shape[-1]), device=y.device)
+        # N(mean, cov) is the posterior for x_{F-1}
+        for t in range(F, T+1):
+            mean, cov = dynamics_model.dynamics_update(
+                mean=mean,
+                cov=cov,
+                u=u[t-1],
+            )
+            pred_a = dynamics_model.get_a(mean)
+            pred_y[t-F] = decoder(pred_a)
+
+        return pred_y
