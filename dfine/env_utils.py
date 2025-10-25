aedce99e2c72d745a1ae3bbbaa59aad71397d462
Milad Heidari <milad.h9999@gmail.com>
2025-10-24 23:14:01 -0700

    Fix: env utils

--- dfine/env_utils.py
+++ dfine/env_utils.py
@@ -5,23 +5,23 @@ from dm_control.suite.wrappers import pixels
 import numpy as np
 
 
-class GymWrapper(object):
+class GymEnv(gym.Env):
     """
-    Gym interface wrapper for dm_control env wrapped by pixels.Wrapper
+    Gymnasium interface wrapper for dm_control env wrapped by pixels.Wrapper
     """
-    metadata = {'render.modes': ['human', 'rgb_array']}
+    metadata = {'render.modes': ['rgb_array']}
     reward_range = (-np.inf, np.inf)
 
     def __init__(self, env):
         self._env = env
-        self._viewer = None
 
-    def __getattr(self, name):
+    def __getattr__(self, name):
         return getattr(self._env, name)
 
     @property
     def observation_space(self):
         obs_spec = self._env.observation_spec()
+        # obs_spec['pixels'].shape is (H, W, C)
         return gym.spaces.Box(0, 255, obs_spec['pixels'].shape, dtype=np.uint8)
 
     @property
@@ -29,63 +29,66 @@ class GymWrapper(object):
         action_spec = self._env.action_spec()
         return gym.spaces.Box(action_spec.minimum, action_spec.maximum, dtype=np.float32)
 
+    def reset(self, *, seed=None, options=None):
+        # Gymnasium will pass seed/options; dm_control reset() doesn't take them.
+        # Optional: if you want reproducibility per-episode, you can reseed the task RNG:
+        if seed is not None and hasattr(self._env, "task") and hasattr(self._env.task, "_random"):
+            self._env.task._random = np.random.RandomState(seed)
+        time_step = self._env.reset()
+        obs = time_step.observation['pixels']
+        info = {}
+        return obs, info  # Gymnasium expects (obs, info)
+
     def step(self, action):
         time_step = self._env.step(action)
         obs = time_step.observation['pixels']
-        reward = time_step.reward or 0
-        done = time_step.last()
+        reward = float(time_step.reward) if time_step.reward is not None else 0.0
+        terminated = bool(time_step.last())   # episode ended
+        truncated = False                     # dm_control doesn't report truncation; set as needed
         info = {'discount': time_step.discount}
-        return obs, reward, done, info
-
-    def reset(self):
-        time_step = self._env.reset()
-        obs = time_step.observation['pixels']
-        return obs
+        return obs, reward, terminated, truncated, info
 
     def render(self, mode='rgb_array', **kwargs):
+        if mode != 'rgb_array':
+            raise NotImplementedError("Only rgb_array is supported")
         if not kwargs:
-            kwargs = self._env._render_kwargs
-
+            # pixels.Wrapper stores desired render kwargs here commonly
+            kwargs = getattr(self._env, "_render_kwargs", {"height": 64, "width": 64, "camera_id": 0})
         img = self._env.physics.render(**kwargs)
-        if mode == 'rgb_array':
-            return img
-        else:
-            raise NotImplementedError
+        return img
 
-class RepeatActionWrapper(gym.Wrapper):
+
+class RepeatAction(gym.Wrapper):
     """
-    Action repeat wrapper to act same action repeatedly
+    Action repeat wrapper (Gymnasium API)
     """
     def __init__(self, env, skip=4):
-        gym.Wrapper.__init__(self, env)
+        super().__init__(env=env)
         self._skip = skip
 
-    def reset(self):
-        return self.env.reset()
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
 
     def step(self, action):
         total_reward = 0.0
+        terminated = False
+        truncated = False
+        info = {}
+        obs = None
         for _ in range(self._skip):
-            obs, reward, terminated, truncated, info = self.env.step(action)
-            done = terminated or truncated
-            total_reward += reward
-            if done:
+            obs, r, term, trunc, info = self.env.step(action)
+            total_reward += r
+            terminated = terminated or term
+            truncated = truncated or trunc
+            if terminated or truncated:
                 break
-        return obs, total_reward, done, info
+        return obs, total_reward, terminated, truncated, info
 
 
-def make_env(
-        domain_name: str,
-        task_name: str,
-        seed: int,
-        action_repeat: int=2
-):
-    env = suite.load(
-        domain_name=domain_name,
-        task_name=task_name,
-        task_kwargs={"random": seed}
-    )
+def make_env(domain_name: str, task_name: str, action_repeat: int = 2):
+    env = suite.load(domain_name=domain_name, task_name=task_name)
     env = pixels.Wrapper(env, render_kwargs={"height": 64, "width": 64, "camera_id": 0})
-    env = GymWrapper(env)
-    env = RepeatActionW()
+    env = GymEnv(env=env)
+    env = RepeatAction(env=env, skip=action_repeat)
+    env = RescaleAction(env=env, min_action=-1.0, max_action=1.0)
     return env
