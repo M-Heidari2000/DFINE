72d3ab67618a806f2fd77270da62f01bcd1a4573
Milad Heidari <milad.h9999@gmail.com>
2025-10-25 19:35:23 -0700

    Fix: a serious bug for splitting data

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from datetime import datetime
 from dfine.memory import ReplayBuffer
 from dfine.train import train_backbone
 from dfine.data_loader import load_from_file
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
@@ -76,7 +75,10 @@ if __name__ == "__main__":
     # load the dataset and normalize it
     data_path = Path(args.data_path)
     y, u = load_from_file(data_path=data_path)
-    y_train, y_test, u_train, u_test = train_test_split(y, u, test_size=args.test_ratio)
+    test_size = int(y.shape[0] * args.test_ratio)
+    train_size = y.shape[0] - test_size
+    y_train, y_test = y[:train_size], y[train_size:]
+    u_train, u_test = u[:train_size], u[train_size:]
     y_scaler, u_scaler = StandardScaler(), StandardScaler()
     y_scaler.fit(y_train)
     u_scaler.fit(u_train)
